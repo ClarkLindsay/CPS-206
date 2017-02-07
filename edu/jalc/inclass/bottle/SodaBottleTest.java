6962587eb242cfa09c25a32477588b4a1f03f4b2
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-07 08:49:17 -0600

    refractors path

---
+++ edu/jalc/inclass/bottle/SodaBottleTest.java
@@ -0,0 +1,11 @@
+package edu.jalc.inclass.bottle;
+
+class SodaBottleTest {
+   
+   public static void main(String[] args) {
+      
+      SodaBottle sodaBottle = new SodaBottle();
+      
+      assert(sodaBottle.getCapacity() == 20.0);   
+   }
+}
