77efc6ea0801c4359ae494ab496623178576d39d
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-03-31 08:47:55 -0500

    Lambda stuff

--- interfaces/Bottle.java
+++ interfaces/Bottle.java
@@ -1,12 +1,21 @@
 public class Bottle implements Pourable, Fillable{
 
-   public Bottle(){}
-   public Bottle(int i){}
+   public Bottle(Pourable p){}
+   //public Bottle(Fillable f){}
 
    public void pour(){}
    public void fill(){}
    
    public static void main(String[] args){
-      Bottle bottle = new Bottle();
+      //Bottle bottle = new Bottle();
+      new Bottle(()-> System.out.println("pouring")).pour();
    }
+  
+   //Bottle bottle = 
+      //new Bottle(){
+         //public void pour(){}
+         //public void fill(){}
+      //};
+      
+   
 }
