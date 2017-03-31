43faca7ec25306420e22547261566994c1b7a5d5
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-03-31 08:28:33 -0500

    Constructor stuff

--- interfaces/Bottle.java
+++ interfaces/Bottle.java
@@ -1,5 +1,12 @@
 public class Bottle implements Pourable, Fillable{
 
+   public Bottle(){}
+   public Bottle(int i){}
+
    public void pour(){}
    public void fill(){}
+   
+   public static void main(String[] args){
+      Bottle bottle = new Bottle();
+   }
 }
